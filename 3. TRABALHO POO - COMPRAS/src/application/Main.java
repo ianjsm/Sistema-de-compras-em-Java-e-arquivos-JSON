d860392eeb4fb3952184b8b818318ebc5cbcae83
Ian Soares <ian.soares@aluno.uece.br>
2023-10-26 17:16:15 -0300

    Método add produto

--- 3. TRABALHO POO - COMPRAS/src/application/Main.java
+++ 3. TRABALHO POO - COMPRAS/src/application/Main.java
@@ -1,10 +1,14 @@
 package application;
 
 import java.util.Scanner;
-
+import org.json.JSONObject;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import entities.Administrador;
 import entities.Comprador;
 import entities.Pessoa;
+import entities.Produto;
 import util.Tela;
 
 public class Main {
@@ -24,6 +28,22 @@ public class Main {
 			pessoa = new Administrador(null);
 			Tela.telaInicialAdmin();
 			int opcao_submenu_admin = scan.nextInt();
+			
+			if(opcao_submenu_admin == 1) {
+				Produto.adicionarProduto();
+			}
+			else if(opcao_submenu_admin == 2) {
+				//editar produto
+			}
+			else if(opcao_submenu_admin == 3) {
+				//remov produto
+			}
+			else if(opcao_submenu_admin == 4) {
+				//listar compras
+			}
+			else {
+				System.out.println("Opção inválida!");
+			}
 		}
 		else if(opcao == 2) {
 			pessoa = new Comprador(null);
