5bbb78613dff84772a3c836de1a2c04229ba4deb
Ian Soares <ian.soares@aluno.uece.br>
2023-10-29 14:22:26 -0300

    ...

--- 3. TRABALHO POO - COMPRAS/src/application/Main.java
+++ 3. TRABALHO POO - COMPRAS/src/application/Main.java
@@ -19,57 +19,63 @@ public class Main {
 		while (opcao > 2 || opcao < 1) {
 			System.out.println("1- ADMIN");
 			System.out.println("2- COMPRADOR");
+			System.out.println("3- SAIR DO PROGRAMA");
 			System.out.println("Admin ou comprador?");
 			opcao = scan.nextInt();
 			scan.nextLine();
-		}
 
-		if (opcao == 1) {
+			if (opcao == 1) {
 
-			Pessoa pessoa = new Administrador(null);
+				Pessoa pessoa = new Administrador(null);
 
-			String senha;
-			boolean senhaCorreta = false;
+				String senha;
+				boolean senhaCorreta = false;
 
-			while (!senhaCorreta) {
-				System.out.println("Digite a senha: ");
-				senha = scan.nextLine();
+				while (!senhaCorreta) {
+					System.out.println("Digite a senha: ");
+					senha = scan.nextLine();
 
-				if (senha.equals("admpoo")) {
-					senhaCorreta = true;
-				} else {
-					System.out.println("Senha incorreta! Tente novamente.");
+					if (senha.equals("admpoo")) {
+						senhaCorreta = true;
+					} else {
+						System.out.println("Senha incorreta! Tente novamente.");
+					}
 				}
-			}
 
-			Tela.telaInicialAdmin();
-			int opcao_submenu_admin = scan.nextInt();
+				Tela.telaInicialAdmin();
+				int opcao_submenu_admin = scan.nextInt();
 
-			if (opcao_submenu_admin == 1) {
-				Pessoa.visualizarProdutos();
-			} else if (opcao_submenu_admin == 2) {
-				Administrador.adicionarProduto();
-			} else if (opcao_submenu_admin == 3) {
-				Administrador.editarProduto();
-			} else if (opcao_submenu_admin == 4) {
-				Administrador.removerProduto();
-			} else if (opcao_submenu_admin == 5) {
-				// listar compras realizadas
-			} else if (opcao_submenu_admin == 6) {
-				// retornar ao menu inicial
+				if (opcao_submenu_admin == 1) {
+					Pessoa.visualizarProdutos();
+				} else if (opcao_submenu_admin == 2) {
+					Administrador.adicionarProduto();
+				} else if (opcao_submenu_admin == 3) {
+					Administrador.editarProduto();
+				} else if (opcao_submenu_admin == 4) {
+					Administrador.removerProduto();
+				} else if (opcao_submenu_admin == 5) {
+					// listar compras realizadas
+				} else if (opcao_submenu_admin == 6) {
+					// retornar ao menu inicial
+				} else {
+					System.out.println("Opção inválida!");
+				}
+			} else if (opcao == 2) {
+				Pessoa pessoa = new Comprador(null);
+				Tela.telaInicialComprador();
+				int opcao_submenu_comprador = scan.nextInt();
+
+				if (opcao_submenu_comprador == 1) {
+					Pessoa.visualizarProdutos();
+					Comprador.comprarProduto();
+				}
+			} else if (opcao == 3) {
+				System.out.println("Programa encerrado.");
+				System.exit(0);
 			} else {
 				System.out.println("Opção inválida!");
 			}
-		} else if (opcao == 2) {
-			Pessoa pessoa = new Comprador(null);
-			Tela.telaInicialComprador();
-			int opcao_submenu_comprador = scan.nextInt();
-
-			if (opcao_submenu_comprador == 1) {
-				Pessoa.visualizarProdutos();
-				Comprador.comprarProduto();
-			}
+			scan.close();
 		}
-		scan.close();
 	}
 }
---
+++ 3. TRABALHO POO - COMPRAS/src/entities/Compra.java
@@ -0,0 +1,8 @@
+package entities;
+
+public class Compra {
+	protected int idCompra;
+	//protected Date data e hora;
+	protected String nome;
+	protected double totalVenda;
+}
